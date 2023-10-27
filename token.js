e6024d0f9d505ea63cceef80d76022707cabf409
AndrewHann09 <119769441+AndrewHann09@users.noreply.github.com>
2023-10-27 16:22:22 -0230

    Update token.js

--- token.js
+++ token.js
@@ -83,39 +83,39 @@ function newToken(username) {
 }
 
 function updateToken(argv) {
-    if(DEBUG) console.log('token.updateToken()');
-    if(DEBUG) console.log(argv);
+    if (DEBUG) console.log('token.updateToken()');
+    if (DEBUG) console.log(argv);
     fs.readFile(__dirname + '/json/tokens.json', 'utf-8', (error, data) => {
-        if(error) throw error; 
+        if (error) throw error;
         let tokens = JSON.parse(data);
         tokens.forEach(obj => {
-            if(obj.username === argv[3]) {
-                if(DEBUG) console.log(obj);
-                switch (argv[2]) {
+            if (obj.username === argv[3]) {
+                if (DEBUG) console.log(obj);
+                switch (argv[2].toLowerCase()) { 
                     case 'p':
-                    case 'P':
                         obj.phone = argv[4];
                         break;
                     case 'e':
-                    case 'E':
                         obj.email = argv[4];
                         break;
                     default:
-                        // TODO: handle incorrect options #
+                        console.log(`Invalid option: ${argv[2]}. Please use 'p' for phone or 'e' for email.`);
+                        return; 
                 }
-                if(DEBUG) console.log(obj);
+                if (DEBUG) console.log(obj);
             }
         });
         userTokens = JSON.stringify(tokens);
         fs.writeFile(__dirname + '/json/tokens.json', userTokens, (err) => {
             if (err) console.log(err);
-            else { 
+            else {
                 console.log(`Token record for ${argv[3]} was updated with ${argv[4]}.`);
                 myEmitter.emit('log', 'token.updateToken()', 'INFO', `Token record for ${argv[3]} was updated with ${argv[4]}.`);
             }
-        })
+        });
     });
 }
+
 
 function searchToken(argv, callback) {
     if (DEBUG) console.log('token.searchToken()');
